20b024ae5d198e9e60b36789baaf63bab65ef9a6
Mohamed Nasr <104239430+mohnasr137@users.noreply.github.com>
2024-09-09 13:34:51 +0300

    edit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ except Exception as e:
     model = None
 
 def preprocess_image(img_path):
-    img = load_img(img_path, target_size=(224, 244))
+    img = load_img(img_path, target_size=(224, 224))
     img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0
